c43ba346d69fbdac26d8631e20c192346c520db7
AllRaDi0 <38398665+AllRaDi0@users.noreply.github.com>
2020-09-20 17:24:09 +0300

    Module 4 / task 3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,22 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 1; i < table.rows.length; i++) {
+    table.rows[i].cells[2].innerHTML === "m"
+      ? table.rows[i].classList.add("male")
+      : table.rows[i].classList.add("female");
+
+    table.rows[i].cells[1].innerHTML < 18
+      ? (table.rows[i].style.textDecoration = "line-through")
+      : false;
+
+    table.rows[i].cells[3].dataset.available === "true"
+      ? table.rows[i].classList.add("available")
+      : table.rows[i].classList.add("unavailable");
+
+    table.rows[i].cells[3].hasAttribute("data-available") == false
+      ? table.rows[i].setAttribute("hidden", true)
+      : false;
+  }
+  return table;
 }
