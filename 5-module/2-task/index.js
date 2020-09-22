b2e5e2bdb1857dd5bf53707ba66c649513055d3b
AllRaDi0 <38398665+AllRaDi0@users.noreply.github.com>
2020-09-22 18:24:58 +0300

    added module 5 / task 1-2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector(".toggle-text-button");
+  const textBlock = document.querySelector("#text");
+
+  btn.addEventListener("click", () => {
+    textBlock.hasAttribute("hidden")
+      ? textBlock.removeAttribute("hidden")
+      : textBlock.setAttribute("hidden", true);
+  });
 }
