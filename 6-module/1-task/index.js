07773af889c03e303b9d410c71897ab77b72db22
AllRaDi0 <38398665+AllRaDi0@users.noreply.github.com>
2020-10-02 15:10:40 +0300

    added module 6 / task 1-2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,11 +5,11 @@
  * Пример одного элемента, описывающего строку таблицы
  *
  *      {
-     *          name: '',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *   },
+ *          name: '',
+ *          age: 25,
+ *          salary: '1000',
+ *          city: 'Petrozavodsk'
+ *   },
  *
  * @constructor
  */
@@ -30,5 +30,36 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = this.render(rows);
+  }
+
+  render(rows) {
+    const table = document.createElement("table");
+    const tbody = document.createElement("tbody");
+
+    for (let i = 0; i < rows.length; i++) {
+      const tr = document.createElement("tr");
+      let rowStaff = rows[i];
+
+      for (let key in rowStaff) {
+        let td = document.createElement("td");
+        td.innerHTML = rowStaff[key];
+        tr.append(td);
+      }
+
+      const td = document.createElement("td");
+      const btn = document.createElement("button");
+      btn.innerText = "X";
+      td.addEventListener("click", () => {
+        tr.remove();
+      });
+
+      td.append(btn);
+      tr.append(td);
+      tbody.append(tr);
+    }
+    table.append(tbody);
+
+    return table;
   }
 }
--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,6 +1,67 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class ProductCard {
   constructor(product) {
+    this.elem = this.render(product);
+  }
+
+  createСardTop({ image, price }) {
+    this.cardTop = document.createElement("div");
+    this.cardTop.classList.add("card__top");
+
+    this.topImg = document.createElement("img");
+    this.topImg.classList.add("card__image");
+    this.topImg.setAttribute("src", `/assets/images/products/${image}`);
+    this.topImg.setAttribute("alt", "product");
+    this.cardTop.append(this.topImg);
+
+    this.span = document.createElement("span");
+    this.span.classList.add("card__price");
+    this.span.innerHTML = "€" + price.toFixed(2);
+    this.cardTop.append(this.span);
+  }
+
+  createСardBody({ name }) {
+    this.cardBody = document.createElement("div");
+    this.cardTitle = document.createElement("div");
+    this.cardBtn = document.createElement("button");
+    this.cardImg = document.createElement("img");
+
+    this.cardBody.classList.add("card__body");
+
+    this.cardTitle.classList.add("card__title");
+    this.cardTitle.innerHTML = name;
+
+    this.cardBtn.classList.add("card__button");
+    this.cardBtn.setAttribute("type", "button");
+
+    this.cardImg.setAttribute("alt", "icon");
+    this.cardImg.setAttribute("src", "/assets/images/icons/plus-icon.svg");
+    this.cardBtn.append(this.cardImg);
+    this.cardBody.append(this.cardTitle);
+    this.cardBody.append(this.cardBtn);
+  }
+
+  addProduct({ id }) {
+    this.cardBtn.addEventListener("click", () => {
+      this.eventProductAdd = new CustomEvent("product-add", {
+        detail: id,
+        bubbles: true,
+      });
+      this.card.dispatchEvent(this.eventProductAdd);
+    });
+  }
+
+  render(product) {
+    this.card = document.createElement("div");
+    this.card.classList.add("card");
+
+    this.createСardTop(product);
+    this.createСardBody(product);
+    this.addProduct(product);
+    this.card.append(this.cardTop);
+    this.card.append(this.cardBody);
+
+    return this.card;
   }
 }
